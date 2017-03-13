7aadd72338dd34550e67112a8861c63b6efc79dd
nsokolovskiy <sokolovskynik@gmail.com>
2017-03-13 14:43:25 +0300

    Add ejs. Add examples and img in gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,27 @@ gulp.task('css', function () {
         .pipe(gulp.dest('./build/css'))
 });
 
+gulp.task('img', function () {
+    gulp.src('./src/img/*.*')
+        .pipe(gulp.dest('./build/img'))
+});
+
+gulp.task('html-example', function () {
+    gulp.src('./examples/*.html')
+        .pipe(rigger())
+        .pipe(gulp.dest('./build-examples/'))
+});
+
+gulp.task('css-example', function () {
+    gulp.src('./examples/css/*.css')
+        .pipe(gulp.dest('./build-examples/css'))
+});
+
+gulp.task('img-example', function () {
+    gulp.src('./examples/img/*.*')
+        .pipe(gulp.dest('./build-examples/img'))
+});
+
 gulp.task('watch', function() {
   watch('./src/**/*.html', function(event, cb) {
     gulp.start('html');
@@ -22,6 +43,18 @@ gulp.task('watch', function() {
   watch('./src/css/*.css', function(event, cb) {
     gulp.start('css');
   });
+  watch('./src/img/*.*', function(event, cb) {
+    gulp.start('img');
+  });
+  watch('./examples/**/*.html', function(event, cb) {
+    gulp.start('html-example');
+  });
+  watch('./src/examples/*.css', function(event, cb) {
+    gulp.start('css-example');
+  });
+  watch('./src/examples/*.*', function(event, cb) {
+    gulp.start('img-example');
+  });
 });
 
-gulp.task('default', ['html', 'css', 'watch']);
+gulp.task('default', ['html', 'css', 'watch', 'img', 'html-example', 'css-example', 'img-example']);
