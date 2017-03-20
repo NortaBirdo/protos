acf50819cb9d4fef2a560a587f7823cacd8477b5
nsokolovskiy <sokolovskynik@gmail.com>
2017-03-20 18:10:17 +0300

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,43 @@
 "use strict";
 
 var path = {
-  examples: {
-    html: './examples/*.html',
-    css: './examples/css/*.css',
-    img: './examples/img/*.*',
-    js: './examples/js/*.*'
+  from: {
+    protos: {
+      css: './css/*.css'
+    },
+    src: {
+      html: './src/*.html',
+      css: './src/css/*.css',
+      img: './src/img/*.*',
+      js: './src/js/*.*'
+    },
+    examples: {
+      html: './examples/*.html',
+      css: './examples/css/*.css',
+      img: './examples/img/*.*',
+      js: './npm /js/*.*'
+    }
   },
-  src: {
-    html: './src/*.html',
-    css: './src/css/*.css',
-    img: './src/img/*.*',
-    js: './src/js/*.*'
-  },
-  protos: {
-    css: './css/*.css'
+  dest: {
+    protos_build: {
+      css: './build/css'
+    },
+    protos_examples: {
+      css: './examples/css'
+    },
+    src: {
+      html: './build/',
+      css: './build/css',
+      img: './build/img',
+      js: './build/js'
+    },
+    examples: {
+      html: './build-examples/',
+      css: './build-examples/css',
+      img: './build-examples/img',
+      js:'./build-examples/js'
+    },
+
   }
 }
 
@@ -23,57 +46,56 @@ var rigger = require('gulp-rigger');
 var watch = require('gulp-watch');
 
 gulp.task('html', function () {
-    gulp.src(path.src.html)
+    gulp.src(path.from.src.html)
         .pipe(rigger())
-        .pipe(gulp.dest('./build/'))
+        .pipe(gulp.dest(path.dest.src.html))
 });
 
 gulp.task('css', function () {
-    gulp.src(path.src.css)
-        .pipe(gulp.dest('./build/css'))
+    gulp.src(path.from.src.css)
+        .pipe(gulp.dest(path.dest.src.css))
 });
 
 gulp.task('img', function () {
-    gulp.src(path.src.img)
-        .pipe(gulp.dest('./build/img'))
+    gulp.src(path.from.src.img)
+        .pipe(gulp.dest(path.dest.src.img))
 });
 
 gulp.task('js', function () {
-    gulp.src(path.src.js)
-        .pipe(gulp.dest('./build/js'))
+    gulp.src(path.from.src.js)
+        .pipe(gulp.dest(path.dest.src.js))
 });
 
 gulp.task('html-example', function () {
-    gulp.src(path.examples.html)
+    gulp.src(path.from.examples.html)
         .pipe(rigger())
-        .pipe(gulp.dest('./build-examples/'))
+        .pipe(gulp.dest(path.dest.examples.html))
 });
 
 gulp.task('css-example', function () {
-    gulp.src(path.examples.css)
-        .pipe(gulp.dest('./build-examples/css'))
+    gulp.src(path.from.examples.css)
+        .pipe(gulp.dest(path.dest.examples.css))
 });
 
 gulp.task('img-example', function () {
-    gulp.src(path.examples.img)
-        .pipe(gulp.dest('./build-examples/img'))
+    gulp.src(path.from.examples.img)
+        .pipe(gulp.dest(path.dest.examples.img))
 });
 
 gulp.task('js-example', function () {
-    gulp.src(path.examples.js)
-        .pipe(gulp.dest('./build-examples/js'))
+    gulp.src(path.from.examples.js)
+        .pipe(gulp.dest(path.from.examples.js))
 });
 
 gulp.task('protos', function () {
-    gulp.src(path.protos.css)
-        .pipe(gulp.dest('./build/css'))
+    gulp.src(path.from.protos.css)
+        .pipe(gulp.dest(path.dest.examples.protos_build.css))
 });
 
 gulp.task('protos-example', function () {
-    gulp.src(path.protos.css)
-        .pipe(gulp.dest('./examples/css'))
+    gulp.src(path.from.protos.css)
+        .pipe(gulp.dest(path.dest.examples.protos_examples.css))
 });
-
 
 gulp.task('watch', function() {
   watch('./src/**/*.html', function(event, cb) {
@@ -102,8 +124,10 @@ gulp.task('watch', function() {
   });
   watch('./css/*.css', function(event, cb) {
     gulp.start('protos');
+  });
+  watch('./css/*.css', function(event, cb) {
     gulp.start('protos-example');
   });
 });
 
-gulp.task('default', ['watch', 'html', 'css', 'js', 'img', 'html-example', 'css-example', 'img-example', 'js-example', 'protos']);
+gulp.task('default', ['watch', 'html', 'css', 'js', 'img', 'html-example', 'css-example', 'img-example', 'js-example', 'protos', 'protos-example']);
